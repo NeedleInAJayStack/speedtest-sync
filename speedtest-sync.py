4a6d02eed35756e03f82595adcf63da1d3e69513
Jay Herron <needleinajaystack@protonmail.com>
2022-08-07 09:35:10 -0600

    Adds support to write to SQL

--- speedtest-sync.py
+++ speedtest-sync.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from dotenv import load_dotenv
 import os
 import psycopg2
@@ -12,20 +12,29 @@ db_name = os.getenv('DB_NAME')
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 
-print(db_host)
+current_time = datetime.now()
 
-# s = speedtest.Speedtest()
-# s.get_servers()
-# s.get_best_server()
-# s.download()
-# s.upload()
-# s.results.share()
-# results_dict = s.results.dict()
-# download = results_dict["download"]
-# upload = results_dict["upload"]
+# Measure speed
+speedtester = speedtest.Speedtest()
+speedtester.get_servers()
+speedtester.get_best_server()
+speedtester.download()
+speedtester.upload()
+speedtester.results.share()
+results_dict = speedtester.results.dict()
+download = results_dict["download"]
+upload = results_dict["upload"]
 
-# conn = psycopg2.connect(host=db_host, port=db_port, dbname=db_name, user=db_user, password=db_password)
-# cur = conn.cursor()
+# Write to SQL
+download_point_id = "a60c7956-7592-4095-8c04-ab6cc41e431a"
+upload_point_id = "5350f77a-9e80-4e8e-8b81-047545a97a63"
 
-# cur.execute("INSERT INTO his (pointId, ts, val) VALUES (%s, %s, %s)", ("", datetime.now(), download))
+conn = psycopg2.connect(host=db_host, port=db_port, dbname=db_name, user=db_user, password=db_password)
+cur = conn.cursor()
 
+cur.execute("INSERT INTO his (\"pointId\", ts, value) VALUES (%s, %s, %s)", (download_point_id, current_time, download))
+cur.execute("INSERT INTO his (\"pointId\", ts, value) VALUES (%s, %s, %s)", (upload_point_id, current_time, upload))
+
+conn.commit()
+cur.close()
+conn.close()
